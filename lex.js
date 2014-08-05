9541b038e27a7c56ebe05e747a20dced97468200
Dana <dsniezko@gmail.com>
2014-08-05 13:31:37 -0400

    oops, old file

--- lex.js
+++ lex.js
@@ -1,6 +1,3 @@
-/*  
-	Simple JS Lexer
-*/
 
 var lexer = function(input) {
 
@@ -77,58 +74,3 @@ var lexer = function(input) {
 	addToken("!END","!END");
 	return tokens;
 };
-
-var revPolParse = function (tokens) {
-
-	var step=0;
-
-	console.log("=========== Parsing ===========");
-
-	var nums = tokens.filter(function(x,i) { 
-		if(x.type == 'number') { return x.value; }
-	});
-
-	var ops = tokens.filter(function(x) { 
-		if(x.type =='operator') { return x.value; }
-	});
-
-	console.log(nums,ops);
-
-	var what = nums.reduce(function(prev,curr,index, arr) { 
-		prev+ops[index]+curr;
-	},0);
-
-	var evalOp = { 
-		"+": function (a,b) {
-			return a+b;
-		},
-		"-": function (a,b) {
-			return a-b;
-		},
-		"*": function (a,b) {
-			return a*b;
-		},
-		"/": function (a,b) {
-			return a/b;
-		}
-	};
-
-	var evaluate = function () {
-		for(var q=0; q < ops.length; ops++) {
-			var tmp = evalOp[ops[q]];
-			nums[q];
-		}
-	};
-
-	console.log(what);
-
-};
-
-var test = lexer("300 200 + is a something. Here is another something (3+2)");
-console.log("Tokens:",test);
-
-console.log("Tokens:",lexer("300.0 + 200 + 100 is a number"));
-
-console.log("Reverse Polish Notation, hehe:",lexer("100 200 300 + +"));
-
-console.log(revPolParse(lexer("100 200 300 + +")));
