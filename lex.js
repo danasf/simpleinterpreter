e21549e2f528191dace3f5b45cfee5dceca6dce9
Dana <dsniezko@gmail.com>
2014-08-05 12:23:34 -0400

    parses

--- lex.js
+++ lex.js
@@ -10,27 +10,34 @@ var lexer = function(input) {
 	var i=0;
 	var c;
 
+
 	var isOperator = function (c) { 
 		return c.match(/[+\-*\/\^%=(),]/);
 	};
 
+	// is this thing a number, easy, look for numbers or period
 	var isNumber = function(c) {
-		return c.match(/[0-9]/);
+		return c.match(/[0-9.]/);
 	};
 
+	// does match spach
 	var isWhiteSpace = function (c) { 
 		return c.match(/\s/);
 	};
 
+	// is it not any of above or undef
 	var isIdentifier = function(c) { 
 		return !null && !undefined && !isOperator(c) && !isNumber(c) && !isWhiteSpace(c);
 	};
 
+	// add to tokens
 	var addToken = function(type,value) {
 		tokens.push({ type: type, value: value });
 	};
 
+	// while less than len
 	while(i < input.length) {
+		// read char
 		c = input.charAt(i);
 		// if white space advance
 		if(isWhiteSpace(c)) { i++; }
@@ -67,8 +74,61 @@ var lexer = function(input) {
 		}
 		else { console.log("unknown token"); }
 	}
+	addToken("!END","!END");
 	return tokens;
+};
+
+var revPolParse = function (tokens) {
+
+	var step=0;
+
+	console.log("=========== Parsing ===========");
+
+	var nums = tokens.filter(function(x,i) { 
+		if(x.type == 'number') { return x.value; }
+	});
+
+	var ops = tokens.filter(function(x) { 
+		if(x.type =='operator') { return x.value; }
+	});
+
+	console.log(nums,ops);
+
+	var what = nums.reduce(function(prev,curr,index, arr) { 
+		prev+ops[index]+curr;
+	},0);
+
+	var evalOp = { 
+		"+": function (a,b) {
+			return a+b;
+		},
+		"-": function (a,b) {
+			return a-b;
+		},
+		"*": function (a,b) {
+			return a*b;
+		},
+		"/": function (a,b) {
+			return a/b;
+		}
+	};
+
+	var evaluate = function () {
+		for(var q=0; q < ops.length; ops++) {
+			var tmp = evalOp[ops[q]];
+			nums[q];
+		}
+	};
+
+	console.log(what);
+
 };
 
 var test = lexer("300 200 + is a something. Here is another something (3+2)");
 console.log("Tokens:",test);
+
+console.log("Tokens:",lexer("300.0 + 200 + 100 is a number"));
+
+console.log("Reverse Polish Notation, hehe:",lexer("100 200 300 + +"));
+
+console.log(revPolParse(lexer("100 200 300 + +")));
---
+++ parse.js
@@ -0,0 +1,216 @@
+/* 
+	Simple JS Parser
+
+	Using Top Down Operator Precedence
+
+	* Associate every operator token with a left bound power, operational function
+	* If operator manipulates tokens to LEFT (+), give it a LEFT DENOTATIVE FUNCTION (LED)
+	* If does NOT manipulate token to LEFT (-), give it a NULL DENOTATIVE FUNCTION (NUD). Identifiers and Numbers also get this
+
+
+	http://effbot.org/zone/simple-top-down-parsing.htm
+	http://eli.thegreenplace.net/2010/01/02/top-down-operator-precedence-parsing/
+	http://www.codeproject.com/Articles/345888/How-to-write-a-simple-interpreter-in-JavaScript
+
+*/
+
+var parse = function (tokens) {
+	var parseTree = [];
+	var symbol_table = {};
+	var tokenStep=0;
+
+	// binding powers of various operators
+	// prefix -a, infix - is a-b
+
+	var toPrefix = [
+		{ key:"-",rbp:10 }
+	];
+
+	var toInfix = [
+		{ key:"^", lbp:9, rbp:6 },
+		{ key:"/", lbp:4 },
+		{ key:"*", lbp:4 },
+		{ key:"%", lbp:4 },
+		{ key:"+",lbp:3 },
+		{ key:"-",lbp:3 }
+	];
+
+	var toSymbol = 
+	[
+		{ key:",", nud:null},
+		{ key:")", nud:null },
+		{ key:"!END", nud:null },
+		{ key:"(", nud: function() { parseParen(); } },
+		{ key:"number", nud: function(num) { return num; } }
+
+	];
+
+
+
+
+	var getToken = function () { 
+		return interpretToken(tokens[tokenStep]); 
+	};
+
+	var advanceToken = function() {
+		tokenStep++;
+		return getToken();
+	};
+
+	/* 
+		http://eli.thegreenplace.net/2010/01/02/top-down-operator-precedence-parsing/
+
+		lbp - left binding power (operator precedence)
+		led - left denotative function
+		nud - null denotative function, 
+	*/
+	var symbol = function(id, nud, lbp, led) {
+		
+		// symbol = look up symbol by type in table, OR new object
+		var sym = symbol_table[id] || {};
+		
+		// lbp - binding power
+		// nud - null denote
+		// led - left denote
+		symbol_table[id] = { 
+			lbp: sym.lbp || lbp, 
+			nud: sym.nud || nud,
+			led: sym.led || led
+		};
+
+	};
+
+	// reads in a token, creates a symbol
+	var interpretToken = function (token) {
+		var sym;
+		if(token.type === undefined) { token.type = null; }
+		if(token.type === "operator") {
+			console.log("Now interpreting:", token.type,symbol_table[token.value]);
+			sym = Object.create(symbol_table[token.value]);
+		}
+		else { 		
+			console.log("Now interpreting:", token.type,symbol_table[token.type]);
+			sym = Object.create(symbol_table[token.type]); 
+		}
+		sym.type = token.type;
+		sym.value = token.value;
+		return sym;
+	};
+
+	// TDOP algorithm
+	var expression = function (rbp) {
+		
+		// set RBP to default of 2 if null
+		//var rbp = rbp || 2;
+		
+		var left;
+		// get a new token and advance
+		var t = getToken();
+		advanceToken();
+		if(!t.nud) { throw "Unknown token type:" + t.type; }
+		// in this new token, set nud of self to left  
+		left = t.nud(t);
+
+		// as long as right binding power < left binding power
+		while(rbp < getToken().lbp) {
+
+			// get a new token and advance
+			t = getToken();
+			advanceToken();
+			if(!t.led) { throw "Unexpected token:" + t.type; }
+			// set left 
+			left = t.led(left);
+		}
+		return left;
+	};
+	
+	// id, left binding, right binding, left deno
+	var infix = function (id, lbp, rbp, led) {
+		// right binding power = rbp if set, else lbp
+		rbp = rbp || lbp;
+		symbol(id, null, lbp, led || function(left) { 
+			return {
+				type: id,
+				left: left,
+				right: expression(rbp)
+			};
+		});
+	}; 
+
+	// id and right binding power only
+	var prefix = function (id, rbp) {
+		symbol(id, function() {
+			return {
+				type: id,
+				right: expression(rbp)
+			};
+		});
+	};
+
+	// NUD helpers for various operators
+	var parseParen = function ()  {
+		var val = expression(2);
+		advanceToken();
+		return value;
+	};
+
+	// identifier
+	var parseIdentifier = function (name) {
+		if(getToken().type === "(") { 
+			var args = [];
+			if (tokens[tokenStep + 1].type === ")") { advanceToken(); }
+			else {
+				do {
+					advanceToken();
+					args.push(expression(2));
+				} while (getToken().type === ",");
+				if(getToken().type !== ")") { throw "We need a closing parenthesis, yo!"; }
+			}
+		advanceToken();
+
+		// return an object
+		return { 
+			type: "call",
+			args: args,
+			name: name.value
+			};
+		}
+		return name;
+	};
+
+	// parse equality
+	var parseEquality = function(left) {
+		if (left.type === "call") {
+			for (var i = 0; i < left.args.length; i++ ) {
+				if ( left.args[i].type !== "identifier") { throw "This argument name isn't valid"; }
+			}
+			return {
+				type: "function",
+				name: left.name,
+				args: left.args,
+				value: expression(2)
+			};
+		} else if ( left.type === "identifier") {
+			return {
+				type: "assign",
+				name: left.value,
+				value: expression(2)
+			};
+		}
+		else { throw "Invalid value"; }
+	};
+
+
+	toInfix.map(function(item) { infix(item.key,item.lbp,item.rbp,item.led); });
+
+	toPrefix.map(function(item) { prefix(item.key,item.rbp); });
+
+	toSymbol.map(function(item) { symbol(item.key,item.nud); });
+
+
+	while(getToken().type !== "!END") {
+		parseTree.push(expression(0));
+	}
+
+	return parseTree;
+};
